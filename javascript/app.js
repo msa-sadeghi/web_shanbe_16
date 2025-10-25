7c8bb27b408d136aaed9f7db713943bba1a00977
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-10-25 16:58:43 +0330

    s

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,33 @@
-let messageDiv = document.getElementById("welcomeMessage")
+// let messageDiv = document.getElementById("t")
 
-function showWelcome(){
-    let userName =  prompt("اسمت رو بگو لطفا")
-    messageDiv.textContent = `سلام ${userName} خوبی؟`
-    messageDiv.style.display = "block"
- 
-}
+// function showWelcome(){
+//     // let userName =  prompt("اسمت رو بگو لطفا")
+//     // messageDiv.textContent = `سلام ${userName} خوبی؟`
+//     // messageDiv.style.display = "block"
+
+//     // let x = Number(prompt("enter the number"))
+//     // let y = Number(prompt("enter the number"))
+//     let x = +prompt("enter the number")
+//     let y = +prompt("enter the number")
+//     console.log(x + y)
+// }
+
+/*
+
+*/
+
+// const x = 12
+
+
+var y = 12
+y = 14
+
+var y
+
+let z = 15
+z = 16
+
+
+// let z
+// let w = 12
+// console.log(w)
