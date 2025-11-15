4647e26a435bfff02ed7f93aaade1678201e1d10
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-11-15 16:53:58 +0330

    s

--- javascript/app.js
+++ javascript/app.js
@@ -1,38 +1,47 @@
+// let x  =0;
+// x = x + 1
+// x += 1
+// x++
+// console.log("123" == 123)
+// console.log("123" === 123)
+// if(x >= 2){
+//    console.log("ff")
+// }
+// else if(x < 1){
+
+// }
+// else{
+
+// }
+// //   &&    ||     !
+// let isLoggedIn = false
+// document.body.innerHTML = isLoggedIn ? "<h1>Hello</h1>" : 
+
+//                                        "<h1>not allowed</h1>"
+
+// if(isLoggedIn){
+//    document.body.innerHTML = "<h1>Welcome</h1>"
+// }else{
+   
+//    document.body.innerHTML = "<h1>not allowed</h1>"
+// }
 
 
-function convertTemperature(){
-    let tempInput = document.getElementById('tempInput').value
-    let tempFrom = document.getElementById('tempFrom').value
-    let tempTo = document.getElementById('tempTo').value
-    let tempResult = document.getElementById('tempResult')
-    let x = Number(tempInput)
-    let result
-    if(tempFrom == tempTo){
-        result = x
-    }else if(tempFrom == 'celsius'){
-         if(tempTo == 'fahrenheit'){
-            result = x * 2
-         }else if(tempTo ==  'kelvin'){
-            result = x * 3
-         }
-    }
-    else if(tempFrom == 'fahrenheit'){
-         if(tempTo == 'celsius'){
-            result = x / 2
-         }else if(tempTo ==  'kelvin'){
-            result = x / 3
-         }
-    }
-    else if(tempFrom == 'kelvin'){
-         if(tempTo == 'fahrenheit'){
-            result = x / 2
-         }else if(tempTo ==  'celsius'){
-            result = x / 3
-         }
-    }
-    tempResult.innerHTML = result
-    tempResult.style.display = "block"
+
+let x = 12
+
+switch(x){
+   case 1:
+   case 5:
+      console.log("blalala")
+      break
+   case 2:
+      console.log("2")
+      break
+   case 12:
+   case 13:
+      console.log("12")
+      break
+   default:
+      console.log("another")
 }
-
-
-
