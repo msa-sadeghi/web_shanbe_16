ce1aab161bb3e6265f20344dfe2a170bfd4dab56
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-12-06 16:25:59 +0330

    S

---
+++ javascript/shopping.js
@@ -0,0 +1,44 @@
+products = []
+
+const createCounter = function(){
+    let id = 0
+    return function(){
+        id++
+        return id
+    }
+}
+
+function addItem(){
+    let itemInputValue = document.getElementById("itemInput").value
+    let p = new Product(itemInputValue, false)
+    products.push(p)
+    renderItems()
+
+}
+let idGenerator = createCounter()
+function Product(name, status){
+    this.id = idGenerator()
+    this.name = name
+    this.status = status   
+}
+
+
+
+function renderItems(){
+    let listSectionElement = document.getElementById("listSection")
+    listSectionElement.innerHTML = ""
+    products.forEach(element => {
+        listSectionElement.innerHTML += `<div class="list-item">
+        <span class="item-number" style="font-size:12px; margin-right:5px">
+        ${element.id}
+        </span>
+        <span class="item-content">
+        ${element.name}
+        </span>
+        </div>`
+    });
+}
+
+function updateStats(){
+
+}
