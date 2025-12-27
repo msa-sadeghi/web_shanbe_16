c4bcda3d5c09a643a2f20de192a7bb5819c08494
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-12-27 16:11:44 +0330

    s

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,31 @@
-let tasks = ["homework", "sleep", "online class", "sport"]
-let itemsDiv = document.getElementById("items")
-let divItems = tasks.map((item) => {
-    return `<div style="color:red; margin:5px">${item}</div>`
-})
-console.log(divItems)
-itemsDiv.innerHTML = divItems.join('')
+let person1 = {
+    name:"sara",
+    age:32,
+    greet(){
+        console.log(`${this.name} is saying Hello`)
+    }
+}
+let person2 = {
+    name:"arash",
+    age:35,
+    greet(){
+        console.log(`${this.name} is saying Hello`)
+    }
+}
+
+person1.greet()
+person2.greet()
+
+function Person(name, age){
+    this.name = name
+    this.age = age
+    this.greet = function(){
+        console.log(`${this.name} is saying Hi`)
+    }
+}
+
+let p1 = new Person("artin", 17)
+p1.greet()
+
+let p2 = new Person("mary", 19)
+p2.greet()
