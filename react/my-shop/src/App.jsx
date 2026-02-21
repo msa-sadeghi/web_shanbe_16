758e196f98db95cd20daf524f137c9e6f5733cf7
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-02-21 16:57:14 +0330

    s

---
+++ react/my-shop/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+     <h1>Hello from react</h1>
+     <p>we want create shop with react</p>
+    </div>
+  )
+}
+
+export default App
