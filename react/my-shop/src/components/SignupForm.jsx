ac387f92804fae9a86b833a9b818c85fd664c65e
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-05-09 16:56:14 +0330

    s

--- react/my-shop/src/components/SignupForm.jsx
+++ react/my-shop/src/components/SignupForm.jsx
@@ -1,19 +1,40 @@
+import { useState } from "react"
+
 function SignupForm(){
-
+    const  [formData, setFormData] = useState({
+        "username":"",
+        "email":"",
+        "password":"",
+        "rePassword":""
+    })
+    const [errors, setErrors] = useState({})
+    const handleChange = (e)=>{
+        setFormData( {...formData,  [e.target.name]:e.target.value})
+    }
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        if(formData.username.length < 3){
+            setErrors({'username':'username length must be greaterh than 3'})
+        }
+    }
     return(
-
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <h2>Signup Form</h2>
-            <input 
+            <input
+            onChange={handleChange} 
             name="username"
             type="text" placeholder="username..." />
+            {errors.username ? errors.username : ''}
             <input 
+            onChange={handleChange} 
             name="email"
             type="email" placeholder="email..." />
             <input 
+            onChange={handleChange} 
             name="password"
             type="password"  placeholder="password"/>
             <input 
+            onChange={handleChange} 
             name="repeatPassword"
             type="password"  placeholder="repeatPassword"/>
             <button type="submit">Signup</button>
