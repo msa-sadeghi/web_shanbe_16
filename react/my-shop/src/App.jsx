f4be346252f604b4c504b58fb19e188ab96b4817
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-09-12 16:20:40 +0330

    end of react router dom

--- react/my-shop/src/App.jsx
+++ react/my-shop/src/App.jsx
@@ -1,10 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+import NotFound from "./components/NotFound";
 import ProductDetail from "./components/ProductDetail";
+
+import Layout from "./components/Layout";
 import "./style.css";
 function App() {
   return (
     <Routes>
-      <Route path="/posts/:id" element={<ProductDetail />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/posts/:id" element={<ProductDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
     </Routes>
   );
 }
