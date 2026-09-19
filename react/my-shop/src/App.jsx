4cab7c34f6ee168ea33870694ec3f977b6f4f347
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-09-19 19:23:19 +0330

    tailwindcss

--- react/my-shop/src/App.jsx
+++ react/my-shop/src/App.jsx
@@ -1,17 +1,10 @@
-import { Route, Routes } from "react-router-dom";
-import NotFound from "./components/NotFound";
-import ProductDetail from "./components/ProductDetail";
-
-import Layout from "./components/Layout";
-import "./style.css";
+import "./App.css";
+import Product from "./components/Product";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/posts/:id" element={<ProductDetail />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <>
+      <Product />
+    </>
   );
 }
 
---
+++ react/my-shop/src/components/Product.jsx
@@ -0,0 +1,3 @@
+export default function Product() {
+  return <h1 className="bg-red-600 text-white px-4 py-2 rounded-lg">Laptop</h1>;
+}
